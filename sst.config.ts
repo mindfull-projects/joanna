f24f86e7ebbb45380ea216c410f2d223a77e33ef
Chandu Sasidharan <mail@chandu-sasidharan.de>
2025-04-05 10:13:52 +0200

    add cloudflare provider to setup custom domain

--- sst.config.ts
+++ sst.config.ts
@@ -2,15 +2,20 @@
 /// <reference path="./.sst/platform/config.d.ts" />
 
 export default $config({
-    app(input) {
-        return {
-            name: 'joanna-website',
-            removal: input?.stage === 'production' ? 'retain' : 'remove',
-            protect: ['production'].includes(input?.stage),
-            home: 'aws',
-        };
-    },
-    async run() {
-        new sst.aws.Nextjs('frontend');
-    },
+  app(input) {
+    return {
+      name: 'joanna-website',
+      removal: input?.stage === 'production' ? 'retain' : 'remove',
+      protect: ['production'].includes(input?.stage),
+      home: 'aws',
+    };
+  },
+  async run() {
+    new sst.aws.Nextjs('frontend', {
+      domain: {
+        name: 'touchtheworld.life',
+        dns: sst.cloudflare.dns(),
+      },
+    });
+  },
 });
